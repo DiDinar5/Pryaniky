a14a0fe38d42838e236616a75f16858218e14123
Dinar <dinar-wp9@mail.ru>
2023-02-25 10:11:59 +0300

    commitPut

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -66,34 +66,34 @@ namespace Pryaniky.Controllers
 
         // PUT: api/Orders/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutOrder(int id, Order order)
-        {
-            if (id != order.Id)
-            {
-                return BadRequest();
-            }
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutOrder(int id, Order order)
+        //{
+        //    if (id != order.Id)
+        //    {
+        //        return BadRequest();
+        //    }
 
-            _context.Entry(order).State = EntityState.Modified;
+        //    _context.Entry(order).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!OrderExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!OrderExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-            return NoContent();
-        } 
+        //    return NoContent();
+        //} 
 
         // DELETE: api/Orders/5
         [HttpDelete("{id}")]
